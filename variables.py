44f753b9653e0f0b83bab0a559725ca6b9804da1
root <root@devx-python001.asomiddin.local>
2020-03-31 02:34:35 -0400

    vars

---
+++ variables.py
@@ -0,0 +1,15 @@
+name = "Asomiddin"
+print(name)
+an_int = 8
+a_float = 2
+the_answer = an_int / a_float
+print(the_answer)
+print(the_answer == an_int / a_float)
+print(3**3+3 > 30)
+print(3**3+3 == 30)
+print("hello" != "Hello")
+a = 2
+b = 4
+c = 9
+print(a + b)
+print(c - a)
