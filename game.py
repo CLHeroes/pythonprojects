2cca0927231f421e0a62092ca7995d3400b013c8
root <root@devx-python001.asomiddin.local>
2020-03-30 22:11:41 -0400

    after change

--- game.py
+++ game.py
@@ -2,12 +2,11 @@
 # Contact root@localhost
 # A fun little python game
 
-#name = input("What is your name?")
-#age = int(input("What is your age?"))
-#your_age_in_hundred_years = age+100
+name = input("What is your name?")
+age = int(input("What is your age?"))
+your_age_in_hundred_years = age+100
 
-#print("Hi, " + name + "!")
-#print("Your age is " + str(age)+ "but in 100 years, you are going to be" + str(your_age_in_hundred_years))
+print("Hi, " + name + "!")
+print("Your age is " + str(age)+ " but in 100 years, you are going to be " + str(your_age_in_hundred_years))
 
-if(True):
-    print("Hello")
+
